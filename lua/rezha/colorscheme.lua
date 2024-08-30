c166450cb5d57a04217287ddaf03af4cadfdb1bb
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-08-30 22:23:45 +0200

    Customize tokyonight

--- lua/rezha/colorscheme.lua
+++ lua/rezha/colorscheme.lua
@@ -10,7 +10,19 @@
 
 local colorScheme = "tokyonight-night"
 
-local success, _ = pcall(vim.cmd, "colorscheme " .. colorScheme)
+local success, tokyonight = pcall(require, "tokyonight")
+if not success then
+  vim.notify("Error loading tokyonight setup")
+  return
+end
+
+tokyonight.setup {
+  on_colors = function(colors)
+    colors.yellow = "#b7f5e9"
+  end
+}
+
+success, _ = pcall(vim.cmd, "colorscheme " .. colorScheme)
 if not success then
   vim.notify("Error loading colorscheme: " .. colorScheme)
   return
