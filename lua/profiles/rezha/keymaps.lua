6542f21ec2cb372908692f182b77b8fcee196e76
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-09-03 18:37:19 +0200

    Should have added these keymaps long time ago: leader+h for horizontal split and leader+v for vertical split

--- lua/profiles/rezha/keymaps.lua
+++ lua/profiles/rezha/keymaps.lua
@@ -42,6 +42,10 @@ keymap('n', '<leader>c', ':Bdelete!<CR>', opts)
 keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 
+-- faster split
+keymap('n', '<leader>h', ':sp<CR>', opts)
+keymap('n', '<leader>v', ':vs<CR>', opts)
+
 -- resize windows
 keymap('n', '<C-m>', ':vertical resize -2<CR>', opts)
 keymap('n', '<C-n>', ':vertical resize +2<CR>', opts)
