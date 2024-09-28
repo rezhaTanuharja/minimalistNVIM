035d1922d82c6be3c8571bb1df9e9f105f94c588
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-09-28 16:59:20 +0200

    Added a keymap to gitsign so diffthis closes faster

--- lua/pluginsetup/gitsigns.lua
+++ lua/pluginsetup/gitsigns.lua
@@ -63,6 +63,7 @@ return {
     vim.keymap.set('n', '<leader>k', ':Gitsigns preview_hunk<CR>', {noremap = true, silent = true})
     vim.keymap.set('n', '<leader>u', ':Gitsigns reset_hunk<CR>', {noremap = true, silent = true})
     vim.keymap.set('n', '<leader>x', ':vert Gitsigns diffthis<CR>', {noremap = true, silent = true})
+    vim.keymap.set('n', '<leader>z', ':wincmd p | q<CR>', {noremap = true, silent = true})
 
   end
 
