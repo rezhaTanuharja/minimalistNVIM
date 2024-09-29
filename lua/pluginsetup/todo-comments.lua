12d5c7fadcfb9338e0415ff39c54001a39cbc543
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-09-29 16:40:14 +0200

    Now todo has 4 letters for all level of attention

--- lua/pluginsetup/todo-comments.lua
+++ lua/pluginsetup/todo-comments.lua
@@ -25,7 +25,7 @@ return {
 
     keywords = {
 
-      WARN = { icon = '', color = 'error' },
+      BUGS = { icon = '', color = 'error' },
       TODO = { icon = '', color = 'warning' },
       NOTE = { icon = '', color = 'hint' },
 
