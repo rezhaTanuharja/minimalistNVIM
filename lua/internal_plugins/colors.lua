c41a23bd7c97c9a6c46ae51d9795a71d5f5f7ea8
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-10-11 23:01:43 +0200

    Finalized that diagnostic hint and info should still have colors

--- lua/internal_plugins/colors.lua
+++ lua/internal_plugins/colors.lua
@@ -26,9 +26,6 @@ local group_styles = {
   ['Search']    = { fg = '#000000', bg = '#777777' },
   ['CurSearch'] = { fg = '#000000', bg = '#aaaaaa' },
 
-  -- ['DiagnosticInfo'] = { fg = '#999999' },
-  -- ['DiagnosticHint'] = { fg = '#999999' },
-
   ['Directory']          = { fg = '#777777', italic = false },
   ['NvimTreeFolderIcon'] = { fg = '#777777', italic = false },
 
