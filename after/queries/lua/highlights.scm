8df9ff9b84a542dd0a0c2dae5f08dc2eb734d803
rezhaTanuharja <rezha.tanuharja@outlook.com>
2024-10-28 20:22:40 +0100

    Added item NOTE to the treesitter highlight

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -2,3 +2,4 @@
 (((comment) @comment (#contains? @comment "TODO")) @comment.warning (#set! priority 200))
 (((comment) @comment (#contains? @comment "WARN")) @comment.warning (#set! priority 200))
 (((comment) @comment (#contains? @comment "BUGS")) @comment.error (#set! priority 200))
+(((comment) @comment (#contains? @comment "NOTE")) @comment.note (#set! priority 200))
