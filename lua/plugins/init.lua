fa135052739d2809c07a10b20a73efa10c26071c
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-11-10 12:36:46 +0100

    Fixed style inconsistenncy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -118,9 +118,9 @@ lazy.setup {
     { import = location .. 'nvim-tree' },
     { import = location .. 'nvim-dap' },
 
-    {import = location .. 'colors'},
-    {import = location .. 'statusline'},
-    {import = location .. 'inteligence'},
-    {import = location .. 'prompt'},
+    { import = location .. 'colors' },
+    { import = location .. 'statusline' },
+    { import = location .. 'inteligence' },
+    { import = location .. 'prompt' },
   },
 }
