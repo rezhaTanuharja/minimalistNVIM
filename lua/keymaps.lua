bece38adb45dc62b3511f561a185b3beb9639ad9
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2024-12-29 20:16:27 +0700

    added keymaps in visual mode to put highlighted text inside matching pairs, e.g., quotes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -114,6 +114,12 @@ local mode_keymaps = {
     ['<leader>i'] = { action = ':s/^/', desc = 'spawn multiple cursors at the start of highlighted lines' },
     ['<leader>a'] = { action = ':s/$/', desc = 'spawn multiple cursors at the end of highlighted lines' },
 
+    ['"'] = { action = 'c""<esc>P', desc = 'put highlighted text inside pairing chars' },
+    ['['] = { action = 'c[]<esc>P', desc = 'put highlighted text inside pairing chars' },
+    ['('] = { action = 'c()<esc>P', desc = 'put highlighted text inside pairing chars' },
+    ["'"] = { action = "c''<esc>P", desc = 'put highlighted text inside pairing chars' },
+    ['`'] = { action = 'c``<esc>P', desc = 'put highlighted text inside pairing chars' },
+
   },
 
 }
