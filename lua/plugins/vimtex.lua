51bdc479867086a4bf0207997e9100df3923e01b
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-01-02 15:32:08 +0700

    Added snippet and changed out directory for vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,7 +9,7 @@ return {
 
     vim.g.vimtex_compiler_method = 'latexmk'
     vim.g.vimtex_compiler_latexmk = {
-      build_dir = '../build',
+      out_dir = 'build',
       options = { '-pdf', '-interaction=nonstopmode', '-synctex=1'},
     }
 
