92be1cb733a42fc2ba7188260c4b4f1a366c78d6
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-04-05 20:46:20 +0200

    Disable pyright's organize imports in favour of ruff

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -18,6 +18,9 @@ return {
   root_markers = { ".git", "pyproject.toml" },
 
   settings = {
+    pyright = {
+      disableOrganizeImports = true,
+    },
     python = {
       analysis = {
         autoImportCompletions= true,
