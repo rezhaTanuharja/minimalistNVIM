c7b735acb477616f6e43beea956706953e0a5a1a
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-04-05 19:34:03 +0200

    Added highlights for FIXMEs

--- queries/python/highlights.scm
+++ queries/python/highlights.scm
@@ -2,6 +2,7 @@
 (((comment) @comment (#contains? @comment "TODO")) @comment.warning (#set! priority 150))
 (((comment) @comment (#contains? @comment "WARN")) @comment.warning (#set! priority 150))
 (((comment) @comment (#contains? @comment "BUGS")) @comment.error (#set! priority 150))
+(((comment) @comment (#contains? @comment "FIXME")) @comment.error (#set! priority 150))
 (((comment) @comment (#contains? @comment "NOTE")) @comment.note (#set! priority 150))
 
 (function_definition
