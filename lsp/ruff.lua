1f6af19dd35e098b51f01d21636b6716c4e5b109
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-04-05 22:21:56 +0200

    Refine ruff settings

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -17,6 +17,37 @@ return {
 
   root_markers = { ".git", "pyproject.toml" },
 
+  init_options = {
+    settings = {
+      organizeImports = false,
+      lint = {
+        extendSelect = {
+          "A",
+          "ANN",
+          "ARG",
+          "B",
+          "COM",
+          "C4",
+          "D",
+          "DOC",
+          "FBT",
+          "I",
+          "ICN",
+          "N",
+          "PERF",
+          "PL",
+          "Q",
+          "RET",
+          "RUF",
+          "SIM",
+          "SLF",
+          "TID",
+          "W",
+        },
+      },
+    },
+  },
+
   settings = {},
 
 }
