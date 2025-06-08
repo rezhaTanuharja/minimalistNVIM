7742296e12e000f193c88f0f35ad661124cb55b0
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-06-08 20:26:41 +0200

    Tidier and more readable lsp setting

--- lsp/vscode-eslint-language-server.lua
+++ lsp/vscode-eslint-language-server.lua
@@ -15,33 +15,41 @@ return {
   end,
 
   settings = {
+
     codeAction = {
       disableRuleComment = {
         enable = true,
         location = "separateLine"
       },
+
       showDocumentation = {
         enable = true
       }
     },
+
     codeActionOnSave = {
       enable = false,
       mode = "all"
     },
+
     experimental = {
       useFlatConfig = false
     },
+
     format = true,
     nodePath = "",
     onIgnoredFiles = "off",
+
     problems = {
       shortenToSingleLine = false
     },
+
     quiet = false,
     rulesCustomizations = {},
     run = "onType",
     useESLintClass = false,
     validate = "on",
+
     workingDirectory = {
       mode = "location"
     }
