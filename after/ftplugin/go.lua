befa36163cd25b324f8718ce549bb96ff292e02c
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-07-27 01:46:55 +0200

    Added DAP config for Go

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -20,6 +20,53 @@ _G.go_env_set = _G.go_env_set or (function()
     vim.lsp.enable("gopls")
   end
 
+  local success, dap = pcall(require, "dap")
+  if not success then
+    vim.notify("failed to load a plugin: dap")
+    return true
+  end
+
+  dap.adapters.delve = function(callback, config)
+    if config.mode == 'remote' and config.request == 'attach' then
+      callback({
+        type = 'server',
+        host = config.host or '127.0.0.1',
+        port = config.port or '38697'
+      })
+    else
+      callback({
+        type = 'server',
+        port = '${port}',
+        executable = {
+          command = 'dlv',
+          args = { 'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap' },
+          detached = vim.fn.has("win32") == 0,
+        }
+      })
+    end
+  end
+
+  dap.configurations.go = {
+    {
+      name = "Attach to Web Server",
+      type = "delve",
+      request = "attach",
+      mode = "local",
+
+      processId = function()
+        return require('dap.utils').pick_process({ filter = "webserver" })
+      end,
+
+      program = "${workspaceFolder}/cmd/webserver",
+    },
+    {
+      name = "Attach to A Headless Delve",
+      type = "delve",
+      request = "attach",
+      mode = "remote",
+    },
+  }
+
   return true
 
 end)()
