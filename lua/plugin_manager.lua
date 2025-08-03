5c8144ae2f5f0d5296a00a5ca3805b184eeb440d
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-08-03 20:53:03 +0200

    formatted

--- lua/plugin_manager.lua
+++ lua/plugin_manager.lua
@@ -9,15 +9,17 @@
 --
 
 local on_stdout = function(_, data)
-  if not data then return end
+	if not data then
+		return
+	end
 
-  for _, line in ipairs(data) do
-    if line ~= "" then
-      vim.schedule(function()
-        vim.api.nvim_echo({{line, "None"}}, false, {})
-      end)
-    end
-  end
+	for _, line in ipairs(data) do
+		if line ~= "" then
+			vim.schedule(function()
+				vim.api.nvim_echo({ { line, "None" } }, false, {})
+			end)
+		end
+	end
 end
 
 local data_site = vim.fn.stdpath("data") .. "/site"
@@ -26,129 +28,110 @@ local plugins_directory = data_site .. "/pack/plugins/opt/"
 local M = {}
 
 local load = function(name)
+	local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
 
-  local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
+	if spec.init and type(spec.init) == "function" then
+		spec.init()
+	end
 
-  if spec.init and type(spec.init) == "function" then
-    spec.init()
-  end
-
-  if spec.config and type(spec.config) == "function" then
-    vim.defer_fn(function()
-      vim.cmd("packadd " .. name)
-      spec.config()
-    end, 50)
-  end
-
+	if spec.config and type(spec.config) == "function" then
+		vim.defer_fn(function()
+			vim.cmd("packadd " .. name)
+			spec.config()
+		end, 50)
+	end
 end
 
 local build = function(cmd, cwd, name)
-  vim.fn.jobstart(
-    cmd,
-    {
-      cwd = cwd,
-      on_exit = function(_, code)
-        if code ~= 0 then
-          vim.api.nvim_err_writeln("Failed to build " .. name .. ".\n")
-          return
-        end
+	vim.fn.jobstart(cmd, {
+		cwd = cwd,
+		on_exit = function(_, code)
+			if code ~= 0 then
+				vim.api.nvim_err_writeln("Failed to build " .. name .. ".\n")
+				return
+			end
 
-        vim.api.nvim_out_write("Built " .. name .. " successfully.\n")
+			vim.api.nvim_out_write("Built " .. name .. " successfully.\n")
 
-        load(name)
-      end,
-    }
-  )
+			load(name)
+		end,
+	})
 end
 
 local install = function(name)
+	local install_dir = plugins_directory .. name
 
-  local install_dir = plugins_directory .. name
+	if vim.uv.fs_stat(install_dir) then
+		load(name)
+		return
+	end
 
-  if vim.uv.fs_stat(install_dir) then
-    load(name)
-    return
-  end
+	local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
 
-  local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
+	vim.fn.jobstart({ "git", "clone", "--depth=1", "https://github.com/" .. spec[1], install_dir }, {
+		on_stdout = on_stdout,
+		on_stderr = on_stdout,
+		on_exit = function(_, code)
+			if code ~= 0 then
+				vim.api.nvim_err_writeln("Failed to install " .. name .. ".\n")
+				return
+			end
 
-  vim.fn.jobstart(
-    { "git", "clone", "--depth=1", "https://github.com/" .. spec[1], install_dir },
-    {
-      on_stdout = on_stdout,
-      on_stderr = on_stdout,
-      on_exit = function(_, code)
-        if code ~= 0 then
-          vim.api.nvim_err_writeln("Failed to install " .. name .. ".\n")
-          return
-        end
+			vim.api.nvim_out_write("Installed " .. name .. " successfully.\n")
 
-        vim.api.nvim_out_write("Installed " .. name .. " successfully.\n")
+			if not spec.build then
+				load(name)
+				return
+			end
 
-        if not spec.build then
-          load(name)
-          return
-        end
-
-        build(spec.build, install_dir, name)
-
-      end,
-    }
-  )
-
+			build(spec.build, install_dir, name)
+		end,
+	})
 end
 
 local update = function(name)
+	local install_dir = plugins_directory .. name
 
-  local install_dir = plugins_directory .. name
+	local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
 
-  local spec = dofile(vim.fn.stdpath("config") .. "/lua/plugins/" .. name .. ".lua")
+	vim.fn.jobstart({ "git", "pull" }, {
+		cwd = install_dir,
+		on_stdout = on_stdout,
+		on_stderr = on_stdout,
+		on_exit = function(_, code)
+			if code ~= 0 then
+				vim.api.nvim_err_writeln("Failed to update " .. name .. ".\n")
+				return
+			end
 
-  vim.fn.jobstart(
-    { "git", "pull" },
-    {
-      cwd = install_dir,
-      on_stdout = on_stdout,
-      on_stderr = on_stdout,
-      on_exit = function(_, code)
-        if code ~= 0 then
-          vim.api.nvim_err_writeln("Failed to update " .. name .. ".\n")
-          return
-        end
+			vim.api.nvim_out_write("Updated " .. name .. " successfully.\n")
 
-        vim.api.nvim_out_write("Updated " .. name .. " successfully.\n")
+			if not spec.build then
+				load(name)
+				return
+			end
 
-        if not spec.build then
-          load(name)
-          return
-        end
-
-        build(spec.build, install_dir, name)
-
-      end,
-    }
-  )
-
+			build(spec.build, install_dir, name)
+		end,
+	})
 end
 
 M.setup = function(opts)
+	if not string.find(vim.o.packpath, data_site, 1, true) then
+		vim.o.packpath = vim.o.packpath .. "," .. data_site
+	end
 
-  if not string.find(vim.o.packpath, data_site, 1, true) then
-    vim.o.packpath = vim.o.packpath .. "," .. data_site
-  end
+	if opts and opts.plugins and type(opts.plugins) == "table" then
+		for _, plugin in ipairs(opts.plugins) do
+			install(plugin)
+		end
 
-  if opts and opts.plugins and type(opts.plugins) == "table" then
-    for _, plugin in ipairs(opts.plugins) do
-      install(plugin)
-    end
-
-    M.update = function()
-      for _, plugin in ipairs(opts.plugins) do
-        update(plugin)
-      end
-    end
-  end
-
+		M.update = function()
+			for _, plugin in ipairs(opts.plugins) do
+				update(plugin)
+			end
+		end
+	end
 end
 
 return M
