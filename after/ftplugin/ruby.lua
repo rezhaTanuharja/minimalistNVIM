285b5da97ead844e180c8775646c8dc0657ca8a8
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-08-06 08:34:59 +0200

    improve ruby debugger setup

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -43,77 +43,13 @@ _G.ruby_env_set = _G.ruby_env_set or (function()
     vim.lsp.enable("rubocop")
   end
 
-  local success, dap = pcall(require, "dap")
+  local success, debug_ruby = pcall(require, "debug_ruby")
   if not success then
-    vim.notify("failed to load a plugin: dap")
+    vim.notify("missing module: debug_ruby")
     return true
   end
-  
-  local base_args = {
-    "exec", "rdbg",
-    "-n",
-    "--open",
-    "--port", "${port}",
-    "-c",
-    "--",
-  }
 
-  dap.adapters.ruby = function(callback, config)
-
-    local executable_args = vim.list_extend({}, base_args) 
-
-    table.insert(executable_args, config.command)
-    vim.list_extend(executable_args, config.args)
-
-    callback({
-      type = "server",
-      host = "127.0.0.1",
-      port = "${port}",
-      executable = {
-        command = "bundle",
-        args = executable_args,
-      },
-      options = {
-        max_retries = 100,
-      },
-    })
-
-  end
-
-  dap.configurations.ruby = {
-    {
-      type = "ruby",
-      name = "[Ruby] Run the current spec file",
-      request = "attach",
-      command = "bundle",
-      args = { "exec", "rspec", "${file}" },
-      localfs = true,
-      justMyCode = true,
-    },
-    {
-      type = "ruby",
-      name = "[Ruby] Run the Rails server",
-      request = "attach",
-      command = "bundle",
-      args = { "exec", "rails", "s" },
-      localfs = true,
-      justMyCode = true,
-    },
-    {
-      type = "ruby",
-      name = "[Ruby] Run a custom command",
-      request = "attach",
-      command = function()
-        return vim.fn.input("Command: ", "bundle")
-      end,
-      args = function()
-        local input = vim.fn.input("Args (space separated): ", "exec rails s")
-        return vim.split(input, "%s+")
-      end,
-      localfs = true,
-      justMyCode = true,
-    },
-  }
+  debug_ruby.setup()
 
   return true
 
