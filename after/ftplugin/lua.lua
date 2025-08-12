f2864bc12d41982b2d990785345ddb680b5d6116
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-08-12 11:25:06 +0200

    add snippets for lua

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,11 @@
+---
+-- @file after/ftplugin/lua.lua
+--
+-- @brief
+-- Lua - specific config
+--
+-- @author Tanuharja, R.A.
+-- @date 2025-08-12
+--
+
+require("snippets").enable_snippets()
---
+++ lua/snippets/lua.lua
@@ -0,0 +1,32 @@
+---
+-- @file lua/snippets/lua.lua
+--
+-- @brief
+-- Snippets for Lua files
+--
+-- @author Tanuharja, R.A.
+-- @date 2024-08-12
+--
+
+local snippets = {
+
+	named_table = {
+		"${1:name} = {",
+		"\t$0",
+		"}",
+	},
+
+	protected_require = {
+		'local ${1:success}, ${2:variable} = pcall(require, "${3:module}")',
+		"if not ${1} then",
+		'\tvim.notify("failed to load a module: $3")',
+		"\treturn",
+		"end",
+	},
+}
+
+for keyword, body in pairs(snippets) do
+	snippets[keyword] = table.concat(body, "\n")
+end
+
+return snippets
