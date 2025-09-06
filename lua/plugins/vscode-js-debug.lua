5772606db475283b578a77e39e07ee36ae8c3e84
Rezha Adrian Tanuharja <rezha.tanuharja@outlook.com>
2025-09-06 13:46:45 +0200

    add docs

--- lua/plugins/vscode-js-debug.lua
+++ lua/plugins/vscode-js-debug.lua
@@ -1,3 +1,13 @@
+---
+-- @file lua/plugins/vscode-js-debug.lua
+--
+-- @brief
+-- The configuration file for the vscode-js-debug
+--
+-- @author Tanuharja, R.A.
+-- @date 2025-08-29
+--
+
 return {
 
 	"microsoft/vscode-js-debug",
